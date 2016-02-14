b37e00c08152d7e0a8bb40d1b748dcb10cbfed7a
Tim Stadler <timstadler905@googlemail.com>
2016-02-14 13:58:51 +0100

    Created project

---
+++ Last.fm-Scrubbler-WPF/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Last.fm_Scrubbler_WPF
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+	}
+}
