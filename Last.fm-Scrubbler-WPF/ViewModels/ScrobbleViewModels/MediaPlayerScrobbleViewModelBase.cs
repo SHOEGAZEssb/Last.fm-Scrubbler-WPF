bbde577fbb8bf3a4f6c48a850ea4c3561c746f70
Tim Stadler <timstadler905@googlemail.com>
2017-08-02 23:34:57 +0200

    Added ViewModel for Spotify Scrobbler
    Added base class for media player scrobblers

---
+++ Last.fm-Scrubbler-WPF/ViewModels/ScrobbleViewModels/MediaPlayerScrobbleViewModelBase.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Last.fm_Scrubbler_WPF.ViewModels.ScrobbleViewModels
+{
+  abstract class MediaPlayerScrobbleViewModelBase : ScrobbleViewModelBase
+  {
+    #region Properties
+
+    /// <summary>
+    /// If the current playing track has been successfully scrobbled.
+    /// </summary>
+    public bool CurrentTrackScrobbled
+    {
+      get { return _currentTrackScrobbled; }
+      protected set
+      {
+        _currentTrackScrobbled = value;
+        NotifyOfPropertyChange(() => CurrentTrackScrobbled);
+      }
+    }
+    private bool _currentTrackScrobbled;
+
+    /// <summary>
+    /// If the current track is loved on Last.fm.
+    /// </summary>
+    public bool CurrentTrackLoved
+    {
+      get { return _currentTrackLoved; }
+      private set
+      {
+        _currentTrackLoved = value;
+        NotifyOfPropertyChange(() => CurrentTrackLoved);
+      }
+    }
+    private bool _currentTrackLoved;
+
+    public abstract string CurrentTrackName { get; }
+    public abstract string CurrentArtistName { get; }
+    public abstract string CurrentAlbumName { get; }
+    public abstract int CurrentTrackLength { get; }
+
+    /// <summary>
+    /// The artwork of the current playing album.
+    /// </summary>
+    public Uri CurrentAlbumArtwork
+    {
+      get { return _currentAlbumArtwork; }
+      protected set
+      {
+        _currentAlbumArtwork = value;
+        NotifyOfPropertyChange(() => CurrentAlbumArtwork);
+      }
+    }
+    private Uri _currentAlbumArtwork;
+
+    public int CountedSeconds
+    {
+      get { return _countedSeconds; }
+      protected set
+      {
+        _countedSeconds = value;
+        NotifyOfPropertyChange(() => CountedSeconds);
+      }
+    }
+    private int _countedSeconds;
+
+    public double PercentageToScrobble
+    {
+      get { return _percentageToScrobble; }
+      set
+      {
+        double rounded = Math.Round(value, 1);
+        if (rounded < 0.5 || rounded > 1.0)
+          throw new ArgumentOutOfRangeException(nameof(value), "Percentage to scrobble must be greater or equal 50% and smaller or equal 100%");
+
+        _percentageToScrobble = rounded;
+      }
+    }
+    private double _percentageToScrobble;
+
+    public int CurrentTrackLengthToScrobble
+    {
+      get { return (int)Math.Ceiling(CurrentTrackLength * PercentageToScrobble); }
+    }
+
+    #endregion Properties
+
+    #region Member
+
+    /// <summary>
+    /// Base URL to Last.fm music objects.
+    /// </summary>
+    protected const string LASTFMURL = "https://www.last.fm/music/";
+
+    #endregion Member
+
+    public abstract void Connect();
+    public abstract void Disconnect();
+
+    protected virtual void UpdateCurrentTrackInfo()
+    {
+      NotifyOfPropertyChange(() => CurrentTrackName);
+      NotifyOfPropertyChange(() => CurrentArtistName);
+      NotifyOfPropertyChange(() => CurrentAlbumName);
+      NotifyOfPropertyChange(() => CurrentTrackLength);
+      NotifyOfPropertyChange(() => CurrentTrackLengthToScrobble);
+      UpdateLovedInfo();
+      UpdateNowPlaying();
+    }
+
+    public async Task SwitchLoveState()
+    {
+      EnableControls = false;
+
+      try
+      {
+        if (CurrentTrackLoved)
+          await MainViewModel.Client.Track.UnloveAsync(CurrentTrackName, CurrentArtistName);
+        else
+          await MainViewModel.Client.Track.LoveAsync(CurrentTrackName, CurrentArtistName);
+
+        await UpdateLovedInfo();
+      }
+      catch (Exception ex)
+      {
+        OnStatusUpdated("Fatal error while loving/unloving track: " + ex.Message);
+      }
+      finally
+      {
+        EnableControls = true;
+      }
+    }
+
+    protected abstract Task UpdateLovedInfo();
+    protected abstract Task UpdateNowPlaying();
+
+    /// <summary>
+    /// Gets the album artwork of the current track.
+    /// </summary>
+    /// <returns>Task.</returns>
+    private async Task FetchAlbumArtwork()
+    {
+      if (CurrentArtistName != null && CurrentAlbumName != null)
+      {
+        var album = await MainViewModel.Client.Album.GetInfoAsync(CurrentArtistName, CurrentAlbumName);
+        CurrentAlbumArtwork = album?.Content.Images.Large;
+      }
+    }
+
+    /// <summary>
+    /// Opens the Last.fm page for the current track.
+    /// </summary>
+    public void TrackClicked()
+    {
+      Process.Start(string.Format(LASTFMURL + "{0}/{1}/{2}", CurrentArtistName.Replace(' ', '+'), CurrentAlbumName.Replace(' ', '+'), CurrentTrackName.Replace(' ', '+')));
+    }
+
+    /// <summary>
+    /// Opens the Last.fm page for the current artist.
+    /// </summary>
+    public void ArtistClicked()
+    {
+      Process.Start(string.Format(LASTFMURL + "{0}", CurrentArtistName.Replace(' ', '+')));
+    }
+
+    /// <summary>
+    /// Opens the Last.fm page for the current track.
+    /// </summary>
+    public void AlbumClicked()
+    {
+      Process.Start(string.Format(LASTFMURL + "{0}/{1}", CurrentArtistName.Replace(' ', '+'), CurrentAlbumName.Replace(' ', '+')));
+    }
+  }
+}
---
+++ Last.fm-Scrubbler-WPF/ViewModels/ScrobbleViewModels/SpotifyScrobbleViewModel.cs
@@ -0,0 +1,177 @@
+﻿using IF.Lastfm.Core.Objects;
+using SpotifyAPI.Local;
+using SpotifyAPI.Local.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Last.fm_Scrubbler_WPF.ViewModels.ScrobbleViewModels
+{
+  class SpotifyScrobbleViewModel : MediaPlayerScrobbleViewModelBase
+  {
+    #region Properties
+
+    public override bool CanScrobble => throw new NotImplementedException();
+
+    public override bool CanPreview => throw new NotImplementedException();
+
+    public override bool EnableControls { get => throw new NotImplementedException(); protected set => throw new NotImplementedException(); }
+
+    public bool IsSpotifyConnected
+    {
+      get { return _isSpotifyConnected; }
+      private set
+      {
+        _isSpotifyConnected = value;
+        NotifyOfPropertyChange(() => IsSpotifyConnected);
+      }
+    }
+
+    public override string CurrentTrackName => _currentResponse?.Track?.TrackResource?.Name;
+
+    public override string CurrentArtistName => _currentResponse?.Track?.ArtistResource?.Name;
+
+    public override string CurrentAlbumName => _currentResponse?.Track?.AlbumResource?.Name;
+
+    public override int CurrentTrackLength => (int)_currentResponse?.Track?.Length;
+
+    private bool _isSpotifyConnected;
+
+    #endregion Properties
+
+    #region Member
+
+    private SpotifyLocalAPI _spotify;
+    private StatusResponse _currentResponse;
+
+    #endregion Member
+
+    public SpotifyScrobbleViewModel()
+    {
+      _spotify = new SpotifyLocalAPI();
+    }
+
+    public override void Connect()
+    {
+      if (IsSpotifyConnected)
+        DisconnectEvents();
+
+      if (!SpotifyLocalAPI.IsSpotifyRunning())
+      {
+        OnStatusUpdated("Error connecting to Spotify: Client not running");
+        return;
+      }
+      if (!SpotifyLocalAPI.IsSpotifyWebHelperRunning())
+      {
+        OnStatusUpdated("Error connecting to Spotify: WebHelper not running");
+        return;
+      }
+
+      if (!_spotify.Connect())
+      {
+        OnStatusUpdated("Error connecting to Spotify: Unknown error");
+        IsSpotifyConnected = false;
+      }
+      else
+      {
+        ConnectEvents();
+        IsSpotifyConnected = true;
+        _currentResponse = _spotify.GetStatus();
+        UpdateCurrentTrackInfo();
+      }
+    }
+
+    private void ConnectEvents()
+    {
+      _spotify.OnTrackChange += _spotify_OnTrackChange;
+      _spotify.OnTrackTimeChange += _spotify_OnTrackTimeChange;
+      _spotify.ListenForEvents = true;
+    }
+
+    private void _spotify_OnTrackTimeChange(object sender, TrackTimeChangeEventArgs e)
+    {
+      CountedSeconds = (int)e.TrackTime;
+
+      if (CountedSeconds == CurrentTrackLengthToScrobble)
+        Scrobble();
+    }
+
+    private void _spotify_OnTrackChange(object sender, TrackChangeEventArgs e)
+    {
+      if (e.NewTrack.TrackResource.Uri != e.OldTrack.TrackResource.Uri)
+        UpdateCurrentTrackInfo();
+
+      CountedSeconds = 0;
+      CurrentTrackScrobbled = false;
+
+      // repeat?
+    }
+
+    private void DisconnectEvents()
+    {
+      _spotify.ListenForEvents = false;
+      _spotify.OnTrackChange -= _spotify_OnTrackChange;
+      _spotify.OnTrackTimeChange -= _spotify_OnTrackTimeChange;
+    }
+
+    /// <summary>
+    /// Can't happen here.
+    /// </summary>
+    public override void Preview()
+    {
+      throw new NotImplementedException();
+    }
+
+    public override async Task Scrobble()
+    {
+      if (CanScrobble && !CurrentTrackScrobbled && !_currentResponse.Track.IsAd())
+      {
+        EnableControls = false;
+
+        try
+        {
+          OnStatusUpdated("Trying to scrobble currently playing track...");
+
+          Scrobble s = new Scrobble(CurrentArtistName, CurrentAlbumName, CurrentTrackName, DateTime.Now)
+          {
+            Duration = TimeSpan.FromSeconds(CurrentTrackLength),
+          };
+
+          var response = await MainViewModel.Scrobbler.ScrobbleAsync(s);
+          if (response.Success)
+          {
+            OnStatusUpdated(string.Format("Successfully scrobbled {0}!", CurrentTrackName));
+            CurrentTrackScrobbled = true;
+          }
+          else
+            OnStatusUpdated("Error while scrobbling!");
+        }
+        catch (Exception ex)
+        {
+          OnStatusUpdated("Fatal error while trying to scrobble currently playing track. Error: " + ex.Message);
+        }
+        finally
+        {
+          EnableControls = true;
+        }
+      }
+    }
+
+    public override void Disconnect()
+    {
+      throw new NotImplementedException();
+    }
+
+    protected override Task UpdateLovedInfo()
+    {
+      throw new NotImplementedException();
+    }
+
+    protected override Task UpdateNowPlaying()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
