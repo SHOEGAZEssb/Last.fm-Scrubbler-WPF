96a7cf2fdaaaee132ddb8cc6bd7b25a318a00504
Tim Stadler <timstadler905@googlemail.com>
2018-01-25 21:30:26 +0100

    Added tests for ManualScrobbleViewModel.CanScrobble

--- Last.fm-Scrubbler-WPF-Test/ScrobblerTests/ManualScrobblerTest.cs
+++ Last.fm-Scrubbler-WPF-Test/ScrobblerTests/ManualScrobblerTest.cs
@@ -11,8 +11,12 @@ namespace Last.fm_Scrubbler_WPF_Test.ScrobblerTests
   [TestFixture]
   class ManualScrobblerTest
   {
+    /// <summary>
+    /// Tests the <see cref="ManualScrobbleViewModel.Scrobble"/>.
+    /// </summary>
+    /// <returns>Task.</returns>
     [Test]
-    public async Task ManualScrobblerScrobbleTest()
+    public async Task ScrobbleTest()
     {
       DateTime expectedTime = DateTime.Now;
       Scrobble expected = new Scrobble("TestArtist", "TestAlbum", "TestTrack", expectedTime) { AlbumArtist = "TestAlbumArtist", Duration = TimeSpan.FromSeconds(30) };
@@ -36,6 +40,82 @@ namespace Last.fm_Scrubbler_WPF_Test.ScrobblerTests
       await vm.Scrobble();
 
       Assert.That(actual.IsEqualScrobble(expected), Is.True);
+    }
+
+    /// <summary>
+    /// Tests the <see cref="ManualScrobbleViewModel.CanScrobble"/> is false
+    /// when no auth is done.
+    /// </summary>
+    [Test]
+    public void CanScrobbleNoAuthTest()
+    {
+      // given: ManualScrobbleViewModel without auth
+      Mock<IAuthScrobbler> scrobblerMock = new Mock<IAuthScrobbler>();
+      scrobblerMock.Setup(s => s.Auth.Authenticated).Returns(false);
+
+      ManualScrobbleViewModel vm = new ManualScrobbleViewModel(null, scrobblerMock.Object)
+      {
+        // set artist and track
+        Artist = "TestArtist",
+        Track = "TestTrack"
+      };
+
+      // then: CanScrobble should be false
+      Assert.That(vm.CanScrobble, Is.False);
+
+      // make sure it really was the auth
+      scrobblerMock.Setup(s => s.Auth.Authenticated).Returns(true);
+      Assert.That(vm.CanScrobble, Is.True);
+    }
+
+    /// <summary>
+    /// Tests the <see cref="ManualScrobbleViewModel.CanScrobble"/> is false
+    /// when no artist is given.
+    /// </summary>
+    [Test]
+    public void CanScrobbleNoArtistTest()
+    {
+      // given: ManualScrobbleViewModel without given artist.
+      Mock<IAuthScrobbler> scrobblerMock = new Mock<IAuthScrobbler>();
+      scrobblerMock.Setup(s => s.Auth.Authenticated).Returns(true);
+
+      ManualScrobbleViewModel vm = new ManualScrobbleViewModel(null, scrobblerMock.Object)
+      {
+        // set track
+        Track = "TestTrack"
+      };
+
+      // then: CanScrobble should be false
+      Assert.That(vm.CanScrobble, Is.False);
+
+      // make sure it really was the artist
+      vm.Artist = "TestArtist";
+      Assert.That(vm.CanScrobble, Is.True);
+    }
+
+    /// <summary>
+    /// Tests the <see cref="ManualScrobbleViewModel.CanScrobble"/> is false
+    /// when no track is given.
+    /// </summary>
+    [Test]
+    public void CanScrobbleNoTrackTest()
+    {
+      // given: ManualScrobbleViewModel without given artist.
+      Mock<IAuthScrobbler> scrobblerMock = new Mock<IAuthScrobbler>();
+      scrobblerMock.Setup(s => s.Auth.Authenticated).Returns(true);
+
+      ManualScrobbleViewModel vm = new ManualScrobbleViewModel(null, scrobblerMock.Object)
+      {
+        // set artist
+        Artist = "TestArtist"
+      };
+
+      // then: CanScrobble should be false
+      Assert.That(vm.CanScrobble, Is.False);
+
+      // make sure it really was the track
+      vm.Track = "TestTrack";
+      Assert.That(vm.CanScrobble, Is.True);
     }
   }
 }
