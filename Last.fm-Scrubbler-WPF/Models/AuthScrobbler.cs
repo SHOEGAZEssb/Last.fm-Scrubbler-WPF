8fcb0c7739c5dbf4a51fd60704b4441e4a28b133
Tim Stadler <timstadler905@googlemail.com>
2018-05-11 11:39:50 +0200

    Make AuthScrobbler and AuthSQLiteScrobbler disposable

--- Last.fm-Scrubbler-WPF/Models/AuthScrobbler.cs
+++ Last.fm-Scrubbler-WPF/Models/AuthScrobbler.cs
@@ -2,43 +2,96 @@
 using IF.Lastfm.Core.Objects;
 using IF.Lastfm.Core.Scrobblers;
 using Scrubbler.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Scrubbler.Models
 {
-  class AuthScrobbler : IAuthScrobbler
+  /// <summary>
+  /// Scrobbler that is authenticated
+  /// via a <see cref="ILastAuth"/>.
+  /// </summary>
+  class AuthScrobbler : IAuthScrobbler, IDisposable
   {
+    #region Properties
+
+    /// <summary>
+    /// Last.fm authentication object.
+    /// </summary>
     public ILastAuth Auth => _scrobbler.Auth;
 
+    /// <summary>
+    /// If the scrobbler caches scrobbles.
+    /// </summary>
     public bool CacheEnabled => _scrobbler.CacheEnabled;
 
+    #endregion Properties
+
+    #region Member
+
+    /// <summary>
+    /// The actual scrobbler.
+    /// </summary>
     private Scrobbler _scrobbler;
 
+    #endregion Member
+
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    /// <param name="auth">Last.fm authentication object.</param>
+    /// <param name="httpClient">HttpClient to use for connections.</param>
+    public AuthScrobbler(ILastAuth auth, HttpClient httpClient = null)
+    {
+      _scrobbler = new Scrobbler(auth, httpClient);
+    }
+
+    /// <summary>
+    /// Gets the cached tracks.
+    /// </summary>
+    /// <returns>Cached tracks.</returns>
     public Task<IEnumerable<Scrobble>> GetCachedAsync()
     {
       return _scrobbler.GetCachedAsync();
     }
 
+    /// <summary>
+    /// Scrobbles the given <paramref name="scrobble"/>.
+    /// </summary>
+    /// <param name="scrobble">Scrobble object to scrobble.</param>
+    /// <returns>Response.</returns>
     public Task<ScrobbleResponse> ScrobbleAsync(Scrobble scrobble)
     {
       return _scrobbler.ScrobbleAsync(scrobble);
     }
 
+    /// <summary>
+    /// Scrobbles the given <paramref name="scrobbles"/>.
+    /// </summary>
+    /// <param name="scrobbles">Collection of scrobble objects to scrobble.</param>
+    /// <returns>Response.</returns>
     public Task<ScrobbleResponse> ScrobbleAsync(IEnumerable<Scrobble> scrobbles)
     {
       return _scrobbler.ScrobbleAsync(scrobbles);
     }
 
+    /// <summary>
+    /// Sends the cached scrobbles.
+    /// </summary>
+    /// <returns>Response.</returns>
     public Task<ScrobbleResponse> SendCachedScrobblesAsync()
     {
       return _scrobbler.SendCachedScrobblesAsync();
     }
 
-    public AuthScrobbler(ILastAuth auth, HttpClient httpClient = null)
+    /// <summary>
+    /// Disposes this scrobbler.
+    /// </summary>
+    public void Dispose()
     {
-      _scrobbler = new Scrobbler(auth, httpClient);
+      _scrobbler.Dispose();
     }
   }
 }
