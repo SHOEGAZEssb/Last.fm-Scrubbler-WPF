90feee1bf11abf9e6ab233a9bf866a012ade79f3
Tim Stadler <timstadler905@googlemail.com>
2018-06-18 15:48:44 +0200

    Comments

--- Last.fm-Scrubbler-WPF/Interfaces/ICanSelectScrobbles.cs
+++ Last.fm-Scrubbler-WPF/Interfaces/ICanSelectScrobbles.cs
@@ -2,12 +2,36 @@
 
 namespace Scrubbler.Interfaces
 {
+  /// <summary>
+  /// Interface for an object that can
+  /// scrobble multiple scrobbles.
+  /// </summary>
+  /// <typeparam name="T">Type of the scrobbable object.</typeparam>
   interface ICanSelectScrobbles<T> where T : IScrobbableObject
   {
+    /// <summary>
+    /// Possible objects to scrobble.
+    /// </summary>
     ObservableCollection<T> Scrobbles { get; }
+
+    /// <summary>
+    /// Marks all scrobbles as "ToScrobble".
+    /// </summary>
     void SelectAll();
+
+    /// <summary>
+    /// Marks all scrobbles as not "ToScrobble".
+    /// </summary>
     void SelectNone();
+
+    /// <summary>
+    /// Gets if all scrobbles can currently be selected.
+    /// </summary>
     bool CanSelectAll { get; }
+
+    /// <summary>
+    /// Gets if all scrobbles can currently be unselected.
+    /// </summary>
     bool CanSelectNone { get; }
   }
 }
