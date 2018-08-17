9eeb51a955c96b41849791fd0dbb150091d90be0
Tim Stadler <timstadler905@googlemail.com>
2018-08-17 13:24:16 +0200

    Comment LastFMClientFactory

--- Last.fm-Scrubbler-WPF/LastFMClientFactory.cs
+++ Last.fm-Scrubbler-WPF/LastFMClientFactory.cs
@@ -2,8 +2,18 @@
 
 namespace Scrubbler
 {
+  /// <summary>
+  /// Factory for creating <see cref="LastFMClient"/>s.
+  /// </summary>
   class LastFMClientFactory : ILastFMClientFactory
   {
+    /// <summary>
+    /// Creates a new LastFMClient.
+    /// </summary>
+    /// <param name="apiKey">API key.</param>
+    /// <param name="apiSecret">API secret.</param>
+    /// <param name="httpClient">HttpClient used for requests.</param>
+    /// <returns>Newly created LastFMClient.</returns>
     public ILastFMClient CreateClient(string apiKey, string apiSecret, HttpClient httpClient = null)
     {
       return new LastFMClient(apiKey, apiSecret, httpClient);
