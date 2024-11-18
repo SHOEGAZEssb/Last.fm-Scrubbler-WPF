4d28daff685694fa0cacdb8d8df95dd30da0466a
Tim Stadler <timstadler905@googlemail.com>
2024-11-18 11:31:16 +0100

    Update nuget packages

--- Last.fm-Scrubbler-WPF/ExtraFunctions/JSONSplitterView.xaml.cs
+++ Last.fm-Scrubbler-WPF/ExtraFunctions/JSONSplitterView.xaml.cs
@@ -7,6 +7,9 @@ namespace Scrubbler.ExtraFunctions
   /// </summary>
   public partial class JSONSplitterView : UserControl
   {
+    /// <summary>
+    /// Constructor.
+    /// </summary>
     public JSONSplitterView()
     {
       InitializeComponent();
